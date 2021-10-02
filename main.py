6fa0e5c50d248158fc117a9e668d6e1d7825e28c
FernandesW <wilfredfernandes562@gmail.com>
2021-10-02 14:59:04 +0530

    checking bla bla merge

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Release Branch, World!</p>"
+    return "<p>Bla blab Release Branch, World!</p>"
 
 
 if __name__ == "__main__":
